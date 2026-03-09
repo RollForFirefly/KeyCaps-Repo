b66a0ba312894f7de59d8949c327471eda93e3c2
Ashton King <kingashtont@gmail.com>
2026-03-10 08:54:51 +1100

    added gitignore, blank sketch

---
+++ BlankSketch/BlankSketch.ino
@@ -0,0 +1,14 @@
+/*
+  Welcome to the KeyCaps Repo!
+  This is just a blank sketch currently. Feel free to put whatever in it, or create a new sketch!
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
