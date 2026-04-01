904ded479aa6791f694f799455c76c718b302eeb
Ashton King <kingashtont@gmail.com>
2026-04-01 18:22:39 +1100

    hot potato management planning

--- HotPotatoManagement/HotPotatoManagement.ino
+++ HotPotatoManagement/HotPotatoManagement.ino
@@ -8,7 +8,14 @@
 // we could make each game its own cpp class and have them inherit from an IGame interface, with functions like Init(), Start(), Repeat(), HandleInput(input A, ...), Display(String lineOne, ...), etc.
 // it may be worthwhile to have a tab/class for shared variables, so that we aren't chewing through the entire memory of the uno. e.g. generic variables like intValOne, strAlpha, uintVar
 // we could (please no) try to cram everything into one ino file. bad idea.
-//
+
+
+// requirements:
+// there should be a global timer/countdown that is maintained between each game. if it runs out, someone goes boom. 
+// timer restarts. time should be written to LED bar.
+// people are bounced from random game to random game. Each time a new game starts, the person should swap. 
+// winning a minigame just results in someone handing the potato off. losing restarts the minigame for that person. 
+// 
 
 void setup() {
   // put your setup code here, to run once:
